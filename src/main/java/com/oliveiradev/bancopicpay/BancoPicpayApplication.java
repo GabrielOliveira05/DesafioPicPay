8c829e22ab025d789161fad6784426c50eeada80
GabrielOliveira05 <gabriel-nerf1@hotmail.com>
2026-02-19 17:15:04 -0300

    feat: initial config

---
+++ src/main/java/com/oliveiradev/bancopicpay/BancoPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.oliveiradev.bancopicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveiradev/bancopicpay/BancoPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiradev.bancopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
