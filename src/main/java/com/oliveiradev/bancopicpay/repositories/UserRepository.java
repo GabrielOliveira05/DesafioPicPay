16a493ac67de10199ba0b1c1cac94a9d65e0fd3f
GabrielOliveira05 <gabriel-nerf1@hotmail.com>
2026-02-19 17:16:42 -0300

    feat: create User repository

---
+++ src/main/java/com/oliveiradev/bancopicpay/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.oliveiradev.bancopicpay.repositories;
+
+import com.oliveiradev.bancopicpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+
+}
