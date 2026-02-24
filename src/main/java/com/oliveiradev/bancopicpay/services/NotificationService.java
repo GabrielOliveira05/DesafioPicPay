b126c8e8c033b76b12e006de8bc85dd0c13b48c6
GabrielOliveira05 <gabriel-nerf1@hotmail.com>
2026-02-24 17:38:24 -0300

    feat: create Notification Service

---
+++ src/main/java/com/oliveiradev/bancopicpay/services/NotificationService.java
@@ -0,0 +1,35 @@
+package com.oliveiradev.bancopicpay.services;
+
+
+import com.oliveiradev.bancopicpay.domain.user.User;
+import com.oliveiradev.bancopicpay.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+
+        String email = user.getEmail();
+
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notiicationResponse= restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+        if(!(notiicationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("Erro ao enviar notificação");
+            throw new Exception("Serviço de notificação está fora do ar");
+
+        }
+    }
+
+
+
+}
