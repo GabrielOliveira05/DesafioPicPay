51b179d0c4026a64592fbf1c17e758e16c58fdb3
GabrielOliveira05 <gabriel-nerf1@hotmail.com>
2026-02-24 18:15:02 -0300

    feat: add Usertype in UserDTO

--- src/main/java/com/oliveiradev/bancopicpay/dtos/UserDTO.java
+++ src/main/java/com/oliveiradev/bancopicpay/dtos/UserDTO.java
@@ -1,6 +1,8 @@
 package com.oliveiradev.bancopicpay.dtos;
 
+import com.oliveiradev.bancopicpay.domain.user.UserType;
+
 import java.math.BigDecimal;
 
-public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password) {
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
 }
